93cb70129599284f00e202846231ed8cc10d9c13
nikita <nikitatuhai@gmail.com>
2018-11-18 12:51:31 +0200

    dz

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-'use strict'
+"use strict";
 
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
 const login = prompt("Введите ваш логин!");
 
-
 const addSucces = "Логин успешно добавлен!";
 const loginBusy = "Такой логин уже используется!";
 const loginError = "Ошибка! Логин должен быть от 4 до 16 символов";
 
-const isLoginValid = (login) => (login.length  >= 4 && login.length  <= 16);
+const isLoginValid = login => login.length >= 4 && login.length <= 16;
 
-const isLoginUnique = (allLogins, login) => (!allLogins.includes(login));
+const isLoginUnique = (allLogins, login) => allLogins.includes(login);
 
-console.log(isLoginUnique(logins, login));
-
-const addLogin = (allLogins,login) => {
-
-  if (isLoginValid(login)) {
-
-    if (isLoginUnique(allLogins, login)) {
-      allLogins.push(login);
-      alert(addSucces);
-    } else {
-      alert(loginBusy);
-    }
-    
-  } else {
-    alert(loginError);
+const addLogin = (allLogins, login) => {
+  if (isLoginUnique(allLogins, login)) {
+    return alert(loginBusy);
   }
+
+  if (!isLoginValid(login)) {
+    return alert(loginError);
+  }
+  allLogins.push(login);
+  return alert(addSucces);
 };
 
-addLogin(logins,login);
-
+addLogin(logins, login);
